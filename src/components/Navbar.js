e5ee4f8b2f21bbec630c800353c57f49418580db
JOEL <volzhia@gmail.com>
2021-06-19 01:23:24 +0300

    add simple navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const NavbarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">Home</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/cards">Cards</NavLink>
+          </NavItem>
+          <NavItem>
+          <NavLink href="/viwever">Viwever</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
